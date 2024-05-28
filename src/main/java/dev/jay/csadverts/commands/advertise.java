45cbeed564467a4d563b152c316ee87c3dea7b94
qwml (Jay) <jeffreybennett@live.co.uk>
2024-05-28 14:43:40 +0100

    updated links

--- src/main/java/dev/jay/csadverts/commands/advertise.java
+++ src/main/java/dev/jay/csadverts/commands/advertise.java
@@ -1,6 +1,11 @@
 package dev.jay.csadverts.commands;
 
 import dev.jay.csadverts.CSAdverts;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
@@ -12,6 +17,7 @@ import org.bukkit.entity.Player;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
@@ -20,61 +26,68 @@ import java.util.regex.Pattern;
 public class advertise implements CommandExecutor {
 
     private final CSAdverts plugin;
-    public advertise(CSAdverts plugin){
+
+    public advertise(CSAdverts plugin) {
         this.plugin = plugin;
     }
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+        Player player = (Player) commandSender;
+        if (command.getName().equalsIgnoreCase("advertise")) {
 
-        if (command.getName().equalsIgnoreCase("advertise")){
-            Player player = (Player) commandSender;
             String message = null;
-            if (args.length == 0){
+            if (args.length == 0) {
                 try {
                     PreparedStatement checkMessage = plugin.con.GetDb().prepareStatement("SELECT message FROM adverts WHERE playerUUID=?");
                     checkMessage.setString(1, player.getUniqueId().toString());
                     ResultSet checkMess = checkMessage.executeQuery();
-                    if (checkMess.next()){
+                    if (checkMess.next()) {
                         message = checkMess.getString("message");
                     }
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
 
-                if (!(Objects.equals(message, "unset"))){
+
+                if (!(Objects.equals(message, "unset"))) {
 
                     if (plugin.cooldown.containsKey(player.getUniqueId()) && (plugin.cooldown.get(player.getUniqueId())).longValue() > System.currentTimeMillis()) {
 
                         long timeleft = ((plugin.cooldown.get(player.getUniqueId())).longValue() - System.currentTimeMillis()) / 1000L;
-                        String ocmessage= plugin.getConfig().getString("Messages.on-cooldown");
+                        String ocmessage = plugin.getConfig().getString("Messages.on-cooldown");
                         ocmessage = ocmessage.replace("%timeleft%", plugin.tf.formattedTime((int) timeleft));
                         player.sendMessage(Hex(Color(plugin.getConfig().getString("Plugin.Prefix") + ocmessage)));
 
-                    }else{
+                    } else {
                         List<String> announcement = plugin.getConfig().getStringList("onAnnounce.Message");
-
-                        for (String i : announcement){
+                        for (Player p : Bukkit.getOnlinePlayers()) {
+                        for (String i : announcement) {
                             i = i.replace("%player%", player.getName()).replace("%message%", message);
-                            Bukkit.broadcastMessage(Hex(Color(i)));
+
+                                p.sendMessage(Hex(Color(i)));
+
+
+                            }
                         }
+                        plugin.getLogger().info(Hex(Color(message + " sent by -" + player.getName())));
                         player.sendMessage(Hex(Color(plugin.getConfig().getString("Plugin.Prefix") + plugin.getConfig().getString("Messages.advert-sent"))));
 
                         plugin.cd.adduserCooldown(player);
 
                     }
 
-                }else{
+                } else {
 
                     player.sendMessage(Hex(Color(plugin.getConfig().getString("Plugin.Prefix") + plugin.getConfig().getString("Messages.unset-message"))));
 
                 }
 
-            }else if (args[0].equalsIgnoreCase("set")){
+            } else if (args[0].equalsIgnoreCase("set")) {
 
-                if (!(args.length < 2)){
+                if (!(args.length < 2)) {
                     String message1 = "";
-                    for (int i = 1; i < args.length; i++){
+                    for (int i = 1; i < args.length; i++) {
 
                         message1 = message1 + args[i] + " ";
 
@@ -86,41 +99,40 @@ public class advertise implements CommandExecutor {
                     } catch (SQLException e) {
                         throw new RuntimeException(e);
                     }
-                }else{
+                } else {
 
                     player.sendMessage(Hex(Color(plugin.getConfig().getString("Plugin.Prefix") + plugin.getConfig().getString("Messages.empty-message"))));
 
                 }
 
 
-
-            }else if (args[0].equalsIgnoreCase("reload")){
+            } else if (args[0].equalsIgnoreCase("reload")) {
 
                 plugin.rl.onReload(player);
 
+
+
             }
-
         }
-
-
         return true;
     }
 
-    public String Color(String s){
-        ChatColor.translateAlternateColorCodes('&',s);
-        return s;
-    }
-
-    private static final Pattern HEX_PATTERN = Pattern.compile("&(#\\w{6})");
-
-    public static String Hex(String message) {
-        Matcher matcher = HEX_PATTERN.matcher(net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', message));
-        StringBuffer buffer = new StringBuffer();
-
-        while (matcher.find()) {
-            matcher.appendReplacement(buffer, net.md_5.bungee.api.ChatColor.of(matcher.group(1)).toString());
+        public String Color (String s){
+            ChatColor.translateAlternateColorCodes('&', s);
+            return s;
         }
 
-        return matcher.appendTail(buffer).toString();
-    }
+        private static final Pattern HEX_PATTERN = Pattern.compile("&(#\\w{6})");
+
+        public static String Hex (String message){
+            Matcher matcher = HEX_PATTERN.matcher(net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', message));
+            StringBuffer buffer = new StringBuffer();
+
+            while (matcher.find()) {
+                matcher.appendReplacement(buffer, net.md_5.bungee.api.ChatColor.of(matcher.group(1)).toString());
+            }
+
+            return matcher.appendTail(buffer).toString();
+        }
+
 }
